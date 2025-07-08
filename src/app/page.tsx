b5c9c6351a6833b2d591bd8eb2a4d2701bef384e
Pammi <pammisao1234@gmail.com>
2025-07-08 22:34:11 +0530

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="h-screen flex flex-col items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
-        Welcome! 🎉
+        Welcome
       </h1>
       
     </main>
